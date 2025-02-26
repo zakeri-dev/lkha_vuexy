5741d0b23a768428f2d0e9e408a28835b157cffd
Mahmoodreza Zakeri <m.zakeri@jaguars.dev>
2025-02-26 10:58:03 +0330

    Revert "Merge branch 'main' of https://onedev.ai.khi.local/sahem_panel"
    This reverts commit 1f4cca379c558575dabe1654b19a1f92af3996e1, reversing
    changes made to 506a04b1c75b171b0d355701d97307faf04dbf72.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   basePath: process.env.BASEPATH,
-  trailingSlash: true,
   redirects: async () => {
     return [
       {
