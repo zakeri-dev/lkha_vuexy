69f2c9cb382afd37072f34c5e3a55169d5ee3525
Mahmoodreza Zakeri <m.zakeri@jaguars.dev>
2025-02-26 10:23:40 +0330

    .

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   basePath: process.env.BASEPATH,
+  trailingSlash: true,
   redirects: async () => {
     return [
       {
