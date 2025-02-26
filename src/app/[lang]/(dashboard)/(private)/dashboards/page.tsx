c84725649768491d90e4ec929174f90174ec5bce
Mahmoodreza Zakeri <m.zakeri@jaguars.dev>
2025-02-26 11:42:12 +0330

    .

--- src/app/[lang]/(dashboard)/(private)/dashboards/page.tsx
+++ src/app/[lang]/(dashboard)/(private)/dashboards/page.tsx
@@ -26,12 +26,22 @@ const DashboardCRM = async () => {
       <Grid size={{ xs: 12, lg: 8 }}>
         <ContentSlider />
       </Grid>
-      {/* <Grid size={{ xs: 12, sm: 6, lg: 2 }}>
+      {/* <Grid size={{ xs: 12, lg: 2 }}>
         <></>
       </Grid> */}
-      <Grid size={{ xs: 12, sm: 6, lg: 4 }} className='bg-black rounded-lg overflow-hidden p-1'>
+      <Grid size={{ xs: 12, lg: 4 }} className='bg-black rounded-lg overflow-hidden p-1'>
         <iframe
           src='http://192.168.5.176:9180/595d9fe0-5090-4494-97c7-0f994c9741fb.html'
+          width='100%'
+          height='100%'
+          frameBorder='0'
+          allowFullScreen
+          className=' aspect-video rounded-lg'
+        ></iframe>
+      </Grid>
+      <Grid size={{ xs: 12, sm: 6, lg: 12 }} className='bg-black rounded-lg overflow-hidden p-1'>
+        <iframe
+          src='https://google.com'
           width='100%'
           height='100%'
           frameBorder='0'
